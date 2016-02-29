45a578bb97911e606182dac4965109bcefd8ea97
Bill Wheeler <admin@phpdreams.com>
2016-02-29 04:14:20 -0600

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Google Tag Manager Tracking
  */
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -6,6 +6,6 @@
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NF375C');
-/**
+/*
  * End Google Tag Manager
  */
